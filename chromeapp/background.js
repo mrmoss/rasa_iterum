286ec613f6f7e5854136d23c14f66209ecfe6fcd
Mike Moss <mrmoss@alaska.edu>
2015-12-25 02:18:58 -0900

    Added min and max bounds.

--- chromeapp/background.js
+++ chromeapp/background.js
@@ -1,6 +1,17 @@
 function on_launch()
 {
-	chrome.app.window.create('window.html',{'outerBounds':{'width':400,'height':320}});
+	var width=400;
+	var height=320;
+
+	chrome.app.window.create('window.html',
+	{
+		'innerBounds':
+		{
+			'width':width,'height':height,
+			'maxWidth':width,'maxHeight':height,
+			'minWidth':width,'minHeight':height
+		}
+	});
 }
 
 chrome.app.runtime.onLaunched.addListener(on_launch);
